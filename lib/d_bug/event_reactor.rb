5ad9e1e18d7069cf416a5d5d0e6c30093c703828
AlexandrePrates <alexandre.prates@resultadosdigitais.com.br>
2019-08-30 10:20:38 -0300

    sync

---
+++ lib/d_bug/event_reactor.rb
@@ -0,0 +1,39 @@
+module DBug
+
+  # reactor = EventReactor.new(queue, process_runner, serial_conn)
+  # reactor.start
+  # reactor.wait_event
+  class EventReactor
+
+    def initialize(queue:, handler:)
+      @queue = queue
+      @handler = handler
+    end
+
+    def start(block = false)
+      Thread.new do
+        while event = @queue.pop do
+          puts "[#{self.class.to_s}] Queue dispatch #{event.inspect}" if DBug::DEBUG
+          deliver(event)
+        end
+      end
+      thread.join if block
+    end
+
+    def wait_event
+      deliver(event) if event = @queue.pop
+    end
+
+    def stop
+      @queue.close
+    end
+
+    private
+
+    def deliver(event)
+      @handler.dispatch(event)      
+    end
+
+  end
+
+end
